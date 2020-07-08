8038662ff039da6b8d2b981d2181c605a99a6bfd
Shubham Jain <shubhamjainnie.com>
2020-07-08 16:05:25 +0530

    A

---
+++ src/components/Navbar.vue
@@ -0,0 +1,11 @@
+<template>
+   <nav>
+     <div class="nav-wrapper orange">
+         <div class="container">
+             <router-link to="/"
+             class="brand-logo">Employee 
+             Manager</router-link>
+         </div>
+     </div>
+   </nav>
+</template>
---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default
+{
+    apiKey: "AIzaSyAgHV4ps_JKlx97y5CmTFdPCifJpoxdijQ",
+    authDomain: "vuepro-501ad.firebaseapp.com",
+    databaseURL: "https://vuepro-501ad.firebaseio.com",
+    projectId: "vuepro-501ad",
+    storageBucket: "vuepro-501ad.appspot.com",
+    messagingSenderId: "193068850270",
+    appId: "1:193068850270:web:e09daacd24afefd547e8cf"
+}
